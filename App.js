0871b1df1d6efe8658f49196c6168d8c7f2a85a6
selim <selimouadi31@gmail.com>
2023-08-07 13:51:17 +0200

    first react native app

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flex: 2, width: '100%', border: '1px solid black', justifyContent: 'flex-end', flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{...styles.square, backgroundColor: 'red'}}></View>
+      </View>
+      
+      <View style={{width: '100%',flex: 10, backgroundColor: 'blue'}}>
+        <Text style={{color: 'white', fontSize: 30, textAlign: 'center'}}>Titre</Text>
+      </View>
+      <View style={{width: '100%',flex: 1, backgroundColor: 'yellow'}}></View>
     </View>
   );
 }
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ccc',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
   },
+  square: {
+    width: 50,
+    height: 50
+  }
 });
